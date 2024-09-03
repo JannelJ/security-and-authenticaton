3775439a2c6644eac86652fe8f9e29c5a379edf5
JannelJ <janneljones26@gmail.com>
2024-09-03 11:37:04 +0100

    security config

---
+++ src/main/java/org/northcoders/security_and_authentication/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.northcoders.security_and_authentication.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/api/v1/open/greeting").permitAll();
+            auth.requestMatchers("/api/v1/protected/greeting").authenticated();
+        })
+               .formLogin(withDefaults());
+
+        return http.build();
+    }
+}
