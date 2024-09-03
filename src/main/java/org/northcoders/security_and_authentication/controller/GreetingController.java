e9ce3b08b7b2167849b0f67a0ffa3989f574c52d
Cevdet Çağdaş Köse <chatonode@gmail.com>
2024-09-03 15:07:32 +0100

    feat: return pleasant greeting message

--- src/main/java/org/northcoders/security_and_authentication/controller/GreetingController.java
+++ src/main/java/org/northcoders/security_and_authentication/controller/GreetingController.java
@@ -1,9 +1,13 @@
 package org.northcoders.security_and_authentication.controller;
 
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 
 @RestController
@@ -16,8 +20,14 @@ public class GreetingController {
 
     }
     @GetMapping("/protected/greeting")
-    public String friendlyWelcome(){
-        return "What's up!";
+    public String friendlyWelcome(@AuthenticationPrincipal OAuth2User user){
+        Optional<String> username = user.getAttributes().entrySet().stream()
+                .filter(stringObjectEntry -> stringObjectEntry.getKey().equals("login"))
+                .map(stringObjectEntry -> (String) stringObjectEntry.getValue())
+                .findFirst();
+
+
+        return "Hi, " + username.get() + "! What's up?";
     }
 
 
